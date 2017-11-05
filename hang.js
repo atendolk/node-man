43caaae292fcecae0e20247c715efcc20f21fb0b
Anish Tendolkar <anish.tendolkar@yahoo.com>
2017-11-05 14:49:23 -0600

    message2

--- hang.js
+++ hang.js
@@ -11,62 +11,61 @@ prompt.start();
 
 
 game = {
- 	wordBank: ['superman', 'batman', 'flash', 'spiderman', 'clarkkent', 'ironman', 'thor'],
+  wordBank: ['superman', 'batman', 'flash', 'spiderman', 'clarkkent', 'ironman', 'thor'],
   guessesRemaining: 10,
- 	wordsWon: 0,
- 	currentWrd: null,
+  wordsWon: 0,
+  currentWrd: null,
 
- 	startGame: function (wrd) {
- 		this.resetGuesses();
- 		this.currentWrd = new Word(this.wordBank[Math.floor(Math.random()* this.wordBank.length)]);
- 		this.currentWrd.getLet();
- 		this.promptUser();
+  startGame: function(wrd) {
+    this.resetGuesses();
+    this.currentWrd = new Word(this.wordBank[Math.floor(Math.random() * this.wordBank.length)]);
+    this.currentWrd.getLet();
+    this.promptUser();
 
- 	},
+  },
 
- 	resetGuesses: function(){
- 		this.guessesRemaining = 10;
+  resetGuesses: function() {
+    this.guessesRemaining = 10;
     console.log("Your word will be from the following wordbank: superman, batman, flash, spiderman, clarkkent, ironman, thor");
- 	},
+  },
 
- 	promptUser: function(){
- 		var mars = this;
+  promptUser: function() {
+    var mars = this;
 
 
- 		prompt.get(['guessLetter'], function(err, result){
- 			console.log("You guessed: " + result.guessLetter);
- 			var manyGuessed = mars.currentWrd.checkLetter(result.guessLetter);
+    prompt.get(['guessLetter'], function(err, result) {
+      console.log("You guessed: " + result.guessLetter);
+      var manyGuessed = mars.currentWrd.checkLetter(result.guessLetter);
 
- 			if(manyGuessed == 0) {
- 				console.log("WRONG");
- 				mars.guessesRemaining--;
+      if (manyGuessed == 0) {
+        console.log("WRONG");
+        mars.guessesRemaining--;
 
- 			} else {
- 				console.log("CORRECT");
- 					if(mars.currentWrd.findWord()){
- 						console.log("You won!");
-            //console.log(mars.currentWrd);
-            //console.log(this.currentWrd);
-            //console.log(mars.currentWrd.target);
-            console.log("Your superhero word: " + mars.currentWrd.wordRender());
+      } else {
+        console.log("CORRECT");
+        if (mars.currentWrd.findWord()) {
+          console.log("You won!");
+          //console.log(mars.currentWrd);
+          //console.log(this.currentWrd);
+          //console.log(mars.currentWrd.target);
+          console.log("Your superhero word: " + mars.currentWrd.wordRender());
 
- 						return;
- 					}
- 			}
+          return;
+        }
+      }
 
- 			console.log("Guesses remaining: " + mars.guessesRemaining);
+      console.log("Guesses remaining: " + mars.guessesRemaining);
 
- 			if((mars.guessesRemaining > 0) && (mars.currentWrd.found == false)){
- 				mars.promptUser();
- 			}
- 			else if(mars.guessesRemaining == 0){
- 				console.log("Game over. Correct Word ", mars.currentWrd.target);
- 			} else {
- 				console.log(mars.currentWrd.wordRender());
- 			}
- 		});
+      if ((mars.guessesRemaining > 0) && (mars.currentWrd.found == false)) {
+        mars.promptUser();
+      } else if (mars.guessesRemaining == 0) {
+        console.log("Game over. Correct Word ", mars.currentWrd.target);
+      } else {
+        console.log(mars.currentWrd.wordRender());
+      }
+    });
 
- 	}
+  }
 
 
 };
